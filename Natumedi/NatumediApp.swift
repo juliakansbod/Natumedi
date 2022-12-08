efd7a17d5080803d226882c9e4e0064470198e48
Julia Kansbod <juliakansbod@hotmail.com>
2022-12-08 11:14:17 +0100

    Initial Commit

---
+++ Natumedi/NatumediApp.swift
@@ -0,0 +1,17 @@
+//
+//  NatumediApp.swift
+//  Natumedi
+//
+//  Created by Julia Kansbod on 2022-12-08.
+//
+
+import SwiftUI
+
+@main
+struct NatumediApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
